e8df62c71adaf6048fca77511942fd8bcbadd4a0
pathum177 <pathummalsara177@gmail.com>
2025-05-06 15:10:27 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -5,7 +5,7 @@ const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js');
 // video
 
 cmd({ 
-    pattern: "video", 
+    pattern: "ytmp4,video", 
     react: "🎥", 
     desc: "Download Youtube song", 
     category: "main", 
@@ -28,18 +28,17 @@ cmd({
             return reply("Failed to fetch the video. Please try again later.");
         }
         
-        let ytmsg = ` *🎶DARK SHADOW VIDEO DOWNLOADER🎶*
-        
+        let ytmsg = `╭━━━〔 *DARK SHADOW-MD* 〕━━━┈⊷
+┃▸╭───────────
+┃▸┃๏ *VIDEO DOWNLOADER*
+┃▸└───────────···๏
+╰────────────────┈⊷
 ╭━━❐━⪼
-┇✦ *📃Title* -  ${yts.title}
-
-┇✦ *⏳Duration* - ${yts.timestamp}
-
-┇✦ *🗓️Views* -  ${yts.views}
-
-┇✦ *👤Author* -  ${yts.author.name}
-
-┇✦ *🔗Link* -  ${yts.url}
+┇๏ *📃Title* -  ${yts.title}
+┇๏ *⏳Duration* - ${yts.timestamp}
+┇๏ *🗓️Views* -  ${yts.views}
+┇๏ *👤Author* -  ${yts.author.name}
+┇๏ *🔗Link* -  ${yts.url}
 ╰━━❑━⪼`;
 
         // Send video details
@@ -65,7 +64,7 @@ cmd({
 // play
 
 cmd({ 
-     pattern: "song", 
+     pattern: "ytmp3,song", 
      react: "🎶", 
      desc: "Download Youtube song",
      category: "main", 
@@ -88,18 +87,17 @@ const yt = await ytsearch(q);
         return reply("Failed to fetch the audio. Please try again later.");
     }
     
-    let ytmsg = ` *📽️DARK SHADOW SONG DOWNLOADER 📽️*
-    
+    let ytmsg = `╭━━━〔 *DARK SHADOW-MD* 〕━━━┈⊷
+┃▸╭───────────
+┃▸┃๏ *MUSIC DOWNLOADER*
+┃▸└───────────···๏
+╰────────────────┈⊷
 ╭━━❐━⪼
-┇✦ *📃Tital* -  ${yts.title}
-
-┇✦ *⏳Duration* - ${yts.timestamp
-                   
-┇✦ *🗓️Views* -  ${yts.views}
-
-┇✦ *👤Author* -  ${yts.author.name} 
-
-┇✦ *🔗Link* -  ${yts.url}
+┇๏ *📃Tital* -  ${yts.title}
+┇๏ *⏳Duration* - ${yts.timestamp}
+┇๏ *🗓️Views* -  ${yts.views}
+┇๏ *👤Author* -  ${yts.author.name} 
+┇๏ *🔗Link* -  ${yts.url}
 ╰━━❑━⪼
 > *© Pᴏᴡᴇʀᴇᴅ Bʏ ᴅᴀʀᴋ ꜱʜᴀᴅᴏᴡ ♡*`;
 
