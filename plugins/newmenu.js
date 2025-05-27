1a19afc26d92ff72cbf3caf789f6a276e72e01c1
pathum177 <pathummalsara177@gmail.com>
2025-05-27 13:18:56 +0530

    Update newmenu.js

--- plugins/newmenu.js
+++ plugins/newmenu.js
@@ -1,6 +1,8 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const os = require("os")
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
 
 cmd({
     pattern: "setv",
