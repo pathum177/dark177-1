b1f988377081e1cd8253d6d10e927720c497c9a0
pathum177 <pathummalsara177@gmail.com>
2025-05-27 20:16:14 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,6 +1,6 @@
 const { cmd, commands } = require('../command');
 const os = require("os");
-const { runtime } = require('../lib/functions');
+const { system } = require('../lib/functions');
 
 cmd({
     pattern: "system",
@@ -23,22 +23,6 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈└───────────
 ╰──────────────
 `;
-
-        // Send the status message with an image
-        await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/5XJdT7zS/6691.jpg` },  // Image URL
-            caption: status,
-            contextInfo: {
-                mentionedJid: [m.sender],
-                forwardingScore: 999,
-                isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '',
-                    newsletterName: 'DARK-SHADOW',
-                    serverMessageId: 143
-                }
-            }
-        }, { quoted: mek });
 
     } catch (e) {
         console.error("Error in alive command:", e);
